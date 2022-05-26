184bf4606a2e07e42eb4c340224952214c4f860b
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-05-26 17:17:29 -0300

    Requisito obrigatório 8, montagem

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function createBox() {
 }
 createBox();
 
+//7
 const firstColor = document.querySelector(".first");
 const secondColor = document.querySelector(".second");
 const thirdColor = document.querySelector(".third");
@@ -30,3 +31,25 @@ firstColor.addEventListener("click", handleChangeSelect);
 secondColor.addEventListener("click", handleChangeSelect);
 thirdColor.addEventListener("click", handleChangeSelect);
 fourthColor.addEventListener("click", handleChangeSelect);
+
+//8
+let box = document.querySelector("#pixel-board");
+let selected = document.querySelector(".selected");
+let colors = document.querySelectorAll(".color");
+
+box.addEventListener("click", function(event) {
+    let paint;
+    if (colors[0].className.includes("selected")) {
+        paint = "black";
+    }
+    event.target.backgroundColor = "black";
+})
+    box.addEventListener("click", function(event) {
+        let color = window.getComputedStyle(colors);
+        for (let index = 0; index < colors.length; index += 1) {
+            if (colors[index].className.includes("selected")) {
+                paint = color.backgroundColor;
+            }
+        }
+        event.target = paint;
+    })
