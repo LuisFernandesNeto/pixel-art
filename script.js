61529bf5689683fe0260572cf3e68888a8f963c5
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2023-01-15 09:07:26 -0300

    changes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let n = 5;
 let pixelBox = document.querySelector("#pixel-board");
 function createBox() {
     for (let index = 0; index < n; index += 1) {
-        for (let secondIndex = 0; secondIndex < n; secondIndex +=1) {
+        for (let secondIndex = 0; secondIndex < n; secondIndex += 1) {
             let div = document.createElement("div");
             div.className = "pixel";
             div.style.backgroundColor = "white";
@@ -13,18 +13,22 @@ function createBox() {
         }
     }
 }
+
+//10
 createBox();
 
 let button2 = document.getElementById("generate-board");
 let size = document.getElementById("board-size")
 
-button2.addEventListener("click", function() {
-    let n = size.value; 
-        if (n == "") {
-            alert("Board inválido!")
-        }
+button2.addEventListener("click", function () {
+    let n = size.value;
+    if (n == "") {
+        alert("Board inválido!")
+    }
+    /* pixelBox.removeChild(); */
+
     for (let index = 0; index < n; index += 1) {
-        for (let secondIndex = 0; secondIndex < n; secondIndex +=1) {
+        for (let secondIndex = 0; secondIndex < n; secondIndex += 1) {
             let div = document.createElement("div");
             div.className = "pixel";
             div.style.backgroundColor = "white";
@@ -34,45 +38,39 @@ button2.addEventListener("click", function() {
 })
 
 //7
-function handleChangeSelect(event) {
-    const selectEvent = document.querySelector(".selected");
-    selectEvent.classList.remove("selected");
-    event.target.classList.add("selected");
-}
 let colors = document.getElementsByClassName("color");
 let palette = document.querySelector("#color-palette");
 
-palette.addEventListener("click", function(event){
+palette.addEventListener("click", function (event) {
     let add = event.target;
     for (let index = 0; index < colors.length; index += 1) {
-         colors[index].classList.remove("selected");
+        colors[index].classList.remove("selected");
     }
     add.classList.add("selected");
-}) 
+})
 
+//8
 let box = document.getElementById("pixel-board");
 
 let paint;
 
-    box.addEventListener("click", function(event) {
-        for (let index = 0; index < colors.length; index += 1) {
-            if (colors[index].className.includes("selected")) {
-                    console.log(paint = getComputedStyle(colors[index]).backgroundColor);
-                    
-            }
+box.addEventListener("click", function (event) {
+    for (let index = 0; index < colors.length; index += 1) {
+        if (colors[index].className.includes("selected")) {
+            console.log(paint = getComputedStyle(colors[index]).backgroundColor);
         }
-            event.target.style.backgroundColor = paint;    
-    })
+    }
+    event.target.style.backgroundColor = paint;
+})
 
 //9
 
 let button = document.querySelector("#clear-board");
-    button.addEventListener("click", function(event) {
-        let pixel = document.getElementsByClassName("pixel");
-        for (let index = 0; index < pixel.length; index += 1) {
-            pixel[index].style.backgroundColor = "white"
-        }
-        event.target.style.backgroundColor = "white";
-    })
 
-//10
+button.addEventListener("click", function (event) {
+    let pixel = document.getElementsByClassName("pixel");
+    for (let index = 0; index < pixel.length; index += 1) {
+        pixel[index].style.backgroundColor = "white"
+    }
+    event.target.style.backgroundColor = "white";
+})
